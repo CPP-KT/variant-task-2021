790a4719ae3b57eddb05bc3b88843a7716536e85
Lev Dovzhik <lev.dovzhik@corp.vk.com>
2021-11-28 23:22:52 +0300

    Upd init-repo.sh

--- init-repo.sh
+++ init-repo.sh
@@ -6,14 +6,14 @@
 set -euo pipefail
 IFS=$' \t\n'
 
-if git show-ref --verify --quiet refs/heads/feedback; then
+if git remote | grep --quiet upstream; then
     echo "init-repo.sh was already done on this repository."
     exit 1
 fi
 
 if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
     echo "init-repo.sh is intended to work on a freshly created repository."
-    echo "The working copy of this reposity is dirty. Please checkout or"
+    echo "The working copy of this repository is dirty. Please checkout or"
     echo "stash changes and run ./init-repo.sh again."
     exit 1
 fi
